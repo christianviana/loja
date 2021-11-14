8f035a224d475c303ed8f788a74636c93fae63d1
Christian Lopes Viana <christian.viana@gmail.com>
2021-11-14 19:49:49 -0300

    Novos testes

--- src/test/java/br/com/queroquero/loja/api/VendedorResourceTest.java
+++ src/test/java/br/com/queroquero/loja/api/VendedorResourceTest.java
@@ -5,15 +5,23 @@ import static io.restassured.RestAssured.given;
 import org.hamcrest.core.StringContains;
 import org.junit.jupiter.api.Test;
 
+import br.com.queroquero.loja.bo.Vendedor;
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
 class VendedorResourceTest {
-
+    
     @Test
     void testBuscarVendedorExistente() {
-		given().when().get("/api/v1/vendedores/MM").then().statusCode(200)
-				.body(StringContains.containsString("Maria Maria"));
+        Vendedor vendedor = new Vendedor();
+        vendedor.setMatricula("MJ");
+        vendedor.setNome("Maria Joana");
+        
+        // cria a 1a vez e dá 201
+        given().contentType("application/json").body(vendedor).when().post("/api/v1/vendedores").then().statusCode(201);
+        
+        given().when().get("/api/v1/vendedores/MJ").then().statusCode(200)
+                .body(StringContains.containsString("Maria Joana"));
     }
 
 	@Test
@@ -21,14 +29,29 @@ class VendedorResourceTest {
 		given().when().get("/api/v1/vendedores/MM2").then().statusCode(404);
 	}
 
-//	@Test
-//	public void testCriarVendedorExistente() {
-//		given().when().post("/api/v1/vendedores/{matricula}/{nome}", "MM", "Maria Maria").then().statusCode(500);
-//	}
+    @Test
+    void testCriarVendedorExistente() {
+        Vendedor vendedor = new Vendedor();
+        vendedor.setMatricula("MJ");
+        vendedor.setNome("Maria Joana");
+        
+        // cria a 1a vez e dá 201
+        given().contentType("application/json").body(vendedor)
+            .when().post("/api/v1/vendedores").then()
+                .statusCode(201);
+        
+        // cria a 2a vez e dá 409
+        given().contentType("application/json").body(vendedor).when().post("/api/v1/vendedores").then().statusCode(409);
+    }
 
-//	@Test
-//	public void testCriarVendedorNaoExistente() {
-//		given().when().post("/api/v1/vendedores/{matricula}/{nome}", "PP", "Pedro Paulo").then().statusCode(201);
-//	}
+	@Test
+    void testCriarVendedorNaoExistente() {
+        Vendedor vendedor = new Vendedor();
+        vendedor.setMatricula("CR");
+        vendedor.setNome("Cleiton Ramos ");        
+        given().contentType("application/json").body(vendedor)
+            .when().post("/api/v1/vendedores").then()
+            .statusCode(201);
+	}
 
 }
--- src/test/java/br/com/queroquero/loja/api/VendedorResourceUnitTest.java
+++ src/test/java/br/com/queroquero/loja/api/VendedorResourceUnitTest.java
@@ -1,19 +1,28 @@
 package br.com.queroquero.loja.api;
 
+import static io.restassured.RestAssured.given;
+import static org.mockito.Mockito.when;
+
+import org.hamcrest.core.StringContains;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
+import br.com.queroquero.loja.bo.Vendedor;
 import br.com.queroquero.loja.service.VendedorService;
 
 
-public class VendedorResourceUnitTest {
+class VendedorResourceUnitTest {
 
 	@Mock
 	private VendedorService vendedorService;
 
 	@InjectMocks
+    @Spy
 	private VendedorResource vendedorResource;
 
 	@BeforeEach
@@ -21,21 +30,16 @@ public class VendedorResourceUnitTest {
 		MockitoAnnotations.openMocks(this);
 	}
 
-//    @Test
-//	public void testUsuarioExistente() {
-//
-//		Vendedor vendResposta = new Vendedor();
-//		vendResposta.setNome("JJ");
-//		vendResposta.setMatricula("João José");
-//		when(vendedorService.buscarPorMatricula("JJ")).thenReturn(vendResposta);
-//
-//		given().when().get("/api/v1/vendedores/MM").then().statusCode(200)
-//				.body(StringContains.containsString("João José"));
-//    }
-//
-//	@Test
-//	public void testUsuarioNaoExistente() {
-//		given().when().get("/api/v1/vendedores/MM2").then().statusCode(404);
-//	}
+    @Test
+    void testUsuarioExistente() {
+        
+        Vendedor vendResposta = new Vendedor();
+        vendResposta.setNome("JJ");
+        vendResposta.setMatricula("João José");
+        when(vendedorService.buscarPorMatricula(ArgumentMatchers.any())).thenReturn(vendResposta);
+        
+        given().when().get("/api/v1/vendedores/JJ").then().statusCode(200)
+                .body(StringContains.containsString("João José"));
+    }
 
 }
